eec5a9726fd967c1a78b43329349783f6d32b035
DarrenLimChuanS <darrenlimchuans@gmail.com>
2020-12-03 18:54:37 +0800

    [Server] Fixed an addition t in SkeletonApplicationTests.java

--- server/src/test/java/com/example/skeleton/SkeletonApplicationTests.java
+++ server/src/test/java/com/example/skeleton/SkeletonApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SkeletontApplicationTests {
+public class SkeletonApplicationTests {
 
 	@Test
 	public void contextLoads() {
